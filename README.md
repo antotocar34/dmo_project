b06c0eb7383e22065888bcb35e399148087b9de1
Antoine Carnec <antoinecarnec@gmail.com>
2021-12-18 00:02:29 +0100

    Final Commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Repository structure
-- `writeup` contains the tex files
-- `code` contains any code files
+The code for this project is organised as follows.
+- A knapsack class is implemented in `knapsack.py` in the `task6` folder. This class contains the greedy heuristic and 
+  the guroby callback function for the branch and cut algorithm of task 6.
+- In task1 and task6 there are `simulation.py` and `performance_analysis.py` files which simulate knapsack instances and 
+  analyse results of solving these.
--- code/task6/simulation.py
+++ code/task6/simulation.py
@@ -1,6 +1,6 @@
 # %%
 from typing import List
-import random
+from random import randint
 from time import sleep
 import numpy as np
 from timeit import default_timer as timer
@@ -12,40 +12,48 @@ from knapsack import KnapSack
 # %%
 
 
-def generate_random_knapsack1(n=1000):
-    weights = [random.randint(50,100) for _ in range(n)]
+def generate_random_knapsack(n=1000):
+    weights = [randint(50,100) for _ in range(n)]
 
-    profits = [random.randint(50,100) for _ in range(n)]
+    profits = [randint(50,100) for _ in range(n)]
 
-    capacity = random.randint(np.floor((sum(weights)-1)*0.5),np.floor(sum(weights)*0.8))
+    capacity = randint(np.floor((sum(weights)-1)*0.5),np.floor(sum(weights)*0.8))
+
+    knapsack1 = KnapSack(n=n, w=weights, p=profits, c=capacity)
+    knapsack2 = KnapSack(n=n, w=weights, p=profits, c=capacity)
+
+    return knapsack1, knapsack2
+
+def interesting_generate_random_knapsack(n=1000):
+    """
+    Try to generate an instance which does not perform well.
+    """
+
+    # weights:
+
+    j = 55
+    weights = [randint(80, 90) for _ in range(j)] + [randint(40, 70) for _ in range(j+1, n+1)]
+
+    profits = [randint(600, 800) for _ in range(j)] + [randint(100, 200) for _ in range(j+1, n+1)]
+
+    capacity = randint(100, 200)
 
     knapsack1 = KnapSack(n=n, w=weights, p=profits, c=capacity)
     knapsack2 = KnapSack(n=n, w=weights, p=profits, c=capacity)
 
     return knapsack1, knapsack2
 
-def generate_random_knapsack2(n=1000):
-    weights = [random.randint(50,100) for _ in range(n)]
-
-    profits = [random.randint(50,100) for _ in range(n)]
-
-    capacity = random.randint(np.floor((sum(weights)-1)*0.5),np.floor(sum(weights)*0.8))
-
-    knapsack1 = KnapSack(n=n, w=weights, p=profits, c=capacity)
-    knapsack2 = KnapSack(n=n, w=weights, p=profits, c=capacity)
-
-    return knapsack1, knapsack2
 
 def seconds_to_milliseconds(s): return round(s * 1000)
 
-def simulate(N=500, n=10, ):
+def simulate(generator, N=500, n=10, ):
     result_df = pd.DataFrame(columns=["capacity", "weights", 
                                       "profits", "cut_profit", 
                                       "brute_profit", "cut_time", 
                                       "brute_time"])
     for i in range(N):
         print(f"ITERATION NUMBER: {i}")
-        random_ks1, random_ks2 = generate_random_knapsack1(n=n)
+        random_ks1, random_ks2 = generator(n=n)
 
 
         cut_start = timer()
@@ -74,6 +82,8 @@ def simulate(N=500, n=10, ):
     print("All done :)")
     return result_df
 # %%
-result_df = simulate(N=50, n=100)
+result_df = simulate(N=50, n=100, generator=generate_random_knapsack)
+result_interesting_df = simulate(N=50, n=100, generator=interesting_generate_random_knapsack)
 
-result_df.to_pickle("./result.pkl")
+result_df.to_pickle("./result_basic.pkl")
+result_interesting_df.to_pickle("./result_interesting_basic.pkl")
